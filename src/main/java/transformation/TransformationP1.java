555c8c84ef86a48b0d4df35d6b5166edde3ab3a9
Radomir Krawczykiewicz <kotwic4@gmail.com>
2019-12-14 11:26:39 +0100

    adjust transformation 1 to new requirements

--- src/main/java/transformation/TransformationP1.java
+++ src/main/java/transformation/TransformationP1.java
@@ -1,15 +1,14 @@
 package transformation;
 
-import model.GraphEdge;
-import model.InteriorNode;
-import model.ModelGraph;
-import model.Point3d;
-import model.Vertex;
-import model.VertexType;
+import model.*;
 import org.javatuples.Triplet;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TransformationP1 implements Transformation {
 
@@ -25,28 +24,7 @@ public class TransformationP1 implements Transformation {
         if (!interiorNode.isPartitionRequired()) {
             return false;
         }
-        if (getSimpleVertexCount(triangle) != 3 || !checkIfTriangle(triangle)) {
-            return false;
-        }
-
-        Map<String, Vertex> model = mapTriangleVertexesToModel(graph, interiorNode);
-
-        Vertex simpleVertex1 = model.get(VERTEX_MAP_SIMPLE_VERTEX_1_KEY);
-        Vertex simpleVertex2 = model.get(VERTEX_MAP_SIMPLE_VERTEX_2_KEY);
-        Vertex oppositeLongestEdgeVertex = model.get(VERTEX_MAP_SIMPLE_VERTEX_OPPOSITE_LONGEST_EDGE_KEY);
-
-        if (simpleVertex1 == null || simpleVertex2 == null || oppositeLongestEdgeVertex == null) {
-            throw new RuntimeException("Transformation error");
-        }
-
-        GraphEdge longestEdge = graph.getEdgeBetweenNodes(simpleVertex1, simpleVertex2)
-                .orElseThrow(() -> new RuntimeException("Unknown edge id"));
-        GraphEdge shortEdge1 = graph.getEdgeBetweenNodes(oppositeLongestEdgeVertex, simpleVertex1)
-                .orElseThrow(() -> new RuntimeException("Unknown edge id"));
-        GraphEdge shortEdge2 = graph.getEdgeBetweenNodes(oppositeLongestEdgeVertex, simpleVertex2)
-                .orElseThrow(() -> new RuntimeException("Unknown edge id"));
-
-        return (longestEdge.getL() >= shortEdge1.getL() && longestEdge.getL() >= shortEdge2.getL());
+        return getSimpleVertexCount(triangle) == 3 && checkIfTriangle(triangle);
     }
 
     private boolean checkIfTriangle(Triplet<Vertex, Vertex, Vertex> triangle) {
@@ -63,10 +41,6 @@ public class TransformationP1 implements Transformation {
         Vertex simpleVertex2 = model.get(VERTEX_MAP_SIMPLE_VERTEX_2_KEY);
         Vertex oppositeLongestEdgeVertex = model.get(VERTEX_MAP_SIMPLE_VERTEX_OPPOSITE_LONGEST_EDGE_KEY);
 
-        if (simpleVertex1 == null || simpleVertex2 == null || oppositeLongestEdgeVertex == null) {
-            throw new RuntimeException("Transformation error");
-        }
-
         GraphEdge longestEdge = graph.getEdgeBetweenNodes(simpleVertex1, simpleVertex2)
                 .orElseThrow(() -> new RuntimeException("Unknown edge id"));
 
@@ -75,7 +49,7 @@ public class TransformationP1 implements Transformation {
         graph.deleteEdge(simpleVertex1, simpleVertex2);
 
         Vertex insertedVertex = graph.insertVertex(interiorNode.getId(),
-                VertexType.SIMPLE_NODE,
+                longestEdge.getB() ? VertexType.SIMPLE_NODE : VertexType.HANGING_NODE,
                 Point3d.middlePoint(simpleVertex1.getCoordinates(), simpleVertex2.getCoordinates()));
 
         String newEdge1Id = simpleVertex1.getId().concat(insertedVertex.getId());
@@ -98,10 +72,40 @@ public class TransformationP1 implements Transformation {
         return graph;
     }
 
+    private static boolean checkEdge(GraphEdge edge, List<GraphEdge> edges, boolean onB) {
+        return edge.getB() == onB && edges.stream().allMatch(e -> edge.getL() >= e.getL());
+    }
+
+    private static Optional<GraphEdge> getTraingleLongestEdge(ModelGraph modelGraph, InteriorNode interiorNode, boolean onB) {
+        Triplet<Vertex, Vertex, Vertex> triangle = interiorNode.getTriangle();
+        Vertex v1 = triangle.getValue0();
+        Vertex v2 = triangle.getValue1();
+        Vertex v3 = triangle.getValue2();
+
+        GraphEdge edge1 = modelGraph.getEdgeBetweenNodes(v1, v2)
+                .orElseThrow(() -> new RuntimeException("Unknown edge id"));
+        GraphEdge edge2 = modelGraph.getEdgeBetweenNodes(v2, v3)
+                .orElseThrow(() -> new RuntimeException("Unknown edge id"));
+        GraphEdge edge3 = modelGraph.getEdgeBetweenNodes(v1, v3)
+                .orElseThrow(() -> new RuntimeException("Unknown edge id"));
+
+        List<GraphEdge> edges = Stream.of(edge1, edge2, edge3).collect(Collectors.toList());
+
+        List<GraphEdge> filteredEdges = edges.stream().filter(e -> checkEdge(e, edges, onB)).collect(Collectors.toList());
+
+        return edges.stream().filter(e -> checkEdge(e, edges, onB)).findFirst();
+    }
+
+
     private static Map<String, Vertex> mapTriangleVertexesToModel(ModelGraph modelGraph, InteriorNode interiorNode) {
         Map<String, Vertex> triangleModel = new HashMap<>();
 
-        GraphEdge triangleLongestEdge = modelGraph.getTraingleLongestEdge(interiorNode);
+        GraphEdge triangleLongestEdge = getTraingleLongestEdge(modelGraph, interiorNode, true)
+                .orElseGet(() ->
+                        getTraingleLongestEdge(modelGraph, interiorNode, false)
+                                .orElseThrow(() -> new RuntimeException("Unknown edge"))
+                );
+
         triangleModel.put(VERTEX_MAP_SIMPLE_VERTEX_1_KEY, triangleLongestEdge.getNode0());
         triangleModel.put(VERTEX_MAP_SIMPLE_VERTEX_2_KEY, triangleLongestEdge.getNode1());
 
