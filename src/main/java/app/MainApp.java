f3d615d367c2bc19b88cbd4bdaf1e61b4925db37
kotwic4 <kotwic4@gmail.com>
2019-12-20 10:36:38 +0100

    try connect p1 and p2

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -5,10 +5,9 @@ import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.javatuples.Pair;
 import processor.MapProcessingUtil;
-import transformation.Transformation;
-import transformation.TransformationP1;
-import transformation.TransformationP3;
-import transformation.TransformationP7;
+import transformation.*;
+
+import java.util.Collection;
 
 public class MainApp {
 
@@ -118,24 +117,75 @@ public class MainApp {
         return new Pair<>(graph, in1);
     }
 
+    private static ModelGraph task() {
+        ModelGraph graph = new ModelGraph("testGraph");
+        Vertex v1 = graph.insertVertex("v1", VertexType.SIMPLE_NODE, new Point3d(0.0, 0.0, 0.0));
+        Vertex v2 = graph.insertVertex("v2", VertexType.SIMPLE_NODE, new Point3d(10.0, 0.0, 0.0));
+        Vertex v3 = graph.insertVertex("v3", VertexType.SIMPLE_NODE, new Point3d(10.0, 10.0, 0.0));
+        Vertex v4 = graph.insertVertex("v4", VertexType.SIMPLE_NODE, new Point3d(0.0, 10.0, 0.0));
+
+        GraphEdge v1_v2 = graph.insertEdge("e1", v1, v2, true);
+        GraphEdge v2_v3 = graph.insertEdge("e2", v2, v3, true);
+        GraphEdge v3_v4 = graph.insertEdge("e3", v3, v4, true);
+        GraphEdge v4_v1 = graph.insertEdge("e4", v4, v1, true);
+        GraphEdge v2_v4 = graph.insertEdge("e5", v2, v4, false);
+
+        InteriorNode in1 = graph.insertInterior("i1", v1, v2, v4);
+        InteriorNode in2 = graph.insertInterior("i2", v2, v3, v4);
+
+        return graph;
+    }
+
     public static void main(String[] args) {
         BasicConfigurator.configure();
-
-        Pair<ModelGraph, InteriorNode> task = task5();
-        ModelGraph graph = task.getValue0();
-        InteriorNode interiorNode = task.getValue1();
-
-        Transformation t1 = new TransformationP3();
-        log.info(String.format("Condition state for transformation P1: %b", t1.isConditionCompleted(graph, interiorNode)));
+        ModelGraph graph = task();
+        Transformation t1 = new TransformationP1();
+        Transformation t2 = new TransformationP2();
+//        Transformation t3 = new TransformationP1();
+//        Transformation t4 = new TransformationP1();
+//        Transformation t4 = new TransformationP1();
+//        Transformation t4 = new TransformationP1();
+//        log.info(String.format("Condition state for transformation P1: %b", t1.isConditionCompleted(graph, interiorNode)));
 
         graph.display();
+
+        for(int i = 0; i < 5; i++){
+            Collection<InteriorNode> interiorNodes = graph.getInteriors();
+            for(InteriorNode node: interiorNodes){
+                node.setPartitionRequired(true);
+            }
+//            Collection<InteriorNode> interiorNodes = graph.getInteriors();
+            boolean change = true;
+            while(change){
+                change = false;
+                for(InteriorNode node: interiorNodes){
+                    if(t1.isConditionCompleted(graph, node)){
+                        t1.transformGraph(graph,node);
+                        change = true;
+                        break;
+                    }
+                    try{
+                        if(t2.isConditionCompleted(graph, node)){
+                            t2.transformGraph(graph,node);
+                            change = true;
+                            break;
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+
+//            graph.display();
+        }
 
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        t1.transformGraph(graph, interiorNode);
+//        t1.transformGraph(graph, interiorNode);
 
 //        TerrainMap map = new TerrainMap();
 //        map.fillMapWithExampleData();
